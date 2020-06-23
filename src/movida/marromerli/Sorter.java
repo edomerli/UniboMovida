afaadf537ed5ebc29350cfbc1a37f71fc7e15971
edomerli <merliedoardo@gmail.com>
2020-06-23 17:18:53 +0200

    Removed generic signature from class

--- src/movida/marromerli/Sorter.java
+++ src/movida/marromerli/Sorter.java
@@ -3,6 +3,6 @@ package movida.marromerli;
 import java.util.Comparator;
 import java.util.List;
 
-public interface Sorter<T> {
-    void sort(List<T> keys, Comparator<T> comparator);
+public interface Sorter {
+    <T> void sort(List<T> keys, Comparator<T> comparator);
 }
