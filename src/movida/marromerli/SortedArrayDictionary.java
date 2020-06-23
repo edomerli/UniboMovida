ccc5fc6feaba8054009b50710e0c8ca15e6d0e70
Samuele Marro <marrosamuele@gmail.com>
2020-06-23 12:14:21 +0200

    Rimosso javafx.Pair.

--- src/movida/marromerli/SortedArrayDictionary.java
+++ src/movida/marromerli/SortedArrayDictionary.java
@@ -1,7 +1,5 @@
 package movida.marromerli;
 
-import javafx.util.Pair;
-
 import java.lang.reflect.Array;
 import java.util.*;
 
@@ -12,6 +10,24 @@ public class SortedArrayDictionary<K, V> implements Dictionary<K, V> {
     private Integer arrayCount;
     private Integer arraySize;
     private Comparator<K> comparator;
+
+    class SearchResult {
+        private Integer position;
+        private Boolean found;
+
+        public SearchResult(int position, boolean found) {
+            this.position = position;
+            this.found = found;
+        }
+
+        public Boolean getFound() {
+            return found;
+        }
+
+        public Integer getPosition() {
+            return position;
+        }
+    }
 
     public SortedArrayDictionary(Comparator<K> comparator) {
         this.comparator = comparator;
@@ -48,14 +64,14 @@ public class SortedArrayDictionary<K, V> implements Dictionary<K, V> {
     }
 
 
-    private Pair<Boolean, Integer> keyLookup(K k){
+    private SearchResult keyLookup(K k) {
         int min = 0;
         int max = this.arrayCount; //TODO: -1?
         int medium = (min + max) / 2;//TODO: Nome migliore, inizializzazione corretta
         while (min <= max) {
             medium = (min + max) / 2;
             if (this.comparator.compare(k, this.arrayKeys[medium]) == 0) {
-                return new Pair<Boolean, Integer>(true, medium);
+                return new SearchResult(medium, true);
             } else if (this.comparator.compare(k, this.arrayKeys[medium]) > 0) {
                 min = medium;
             } else {
@@ -63,7 +79,7 @@ public class SortedArrayDictionary<K, V> implements Dictionary<K, V> {
             }
         }
         //TODO: Qual è la posizione corretta di inserimento?
-        return new Pair<Boolean, Integer>(false, medium);
+        return new SearchResult(medium, false);
     }
 
     @Override
@@ -72,13 +88,12 @@ public class SortedArrayDictionary<K, V> implements Dictionary<K, V> {
         int min = 0;
         int max = this.arrayCount;
 
-        Pair<Boolean, Integer> lookup = this.keyLookup(key);
+        SearchResult result = this.keyLookup(key);
 
         //TODO: Non mi piace molto getKey()
-        if (lookup.getKey()) {
-            return this.arrayValues[lookup.getValue()];
-        }
-        else{
+        if (result.getFound()) {
+            return this.arrayValues[result.getPosition()];
+        } else {
             //TODO: è comportamento standard?
             return null;
         }
@@ -101,7 +116,7 @@ public class SortedArrayDictionary<K, V> implements Dictionary<K, V> {
             }
         }
 
-        for (int i = this.arrayCount; i > index; i--){
+        for (int i = this.arrayCount; i > index; i--) {
             this.arrayKeys[i] = this.arrayKeys[i - 1];
             this.arrayValues[i] = this.arrayValues[i - 1];
         }
